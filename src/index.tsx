fbdeee48fe190d510487a8ce8fd4142160e946f8
Pavel Koryakin <unclekoin@gmail.com>
2022-07-18 19:46:13 +0300

    feat: add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import MainProvider from './components/context/mainContext';
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <MainProvider>
       <App />
     </MainProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
